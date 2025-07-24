044b42b32b7beb17107a47899373ffaa2916f7ab
alejandro bernal <alejandro_brnal@icloud.com>
2025-07-24 11:18:43 -0600

    Update content.config.js

--- src/content.config.js
+++ src/content.config.js
@@ -14,16 +14,4 @@ const posts = defineCollection({
 	}),
 });
 
-const proyectos = defineCollection({
-	loader: glob({ pattern: "*.md", base: "./projects" }),
-	schema: z.object({
-		title: z.string(),
-		slug: z.string(),
-		description: z.string(),
-		added: z.union([z.string(), z.date()]),
-		updated: z.union([z.string(), z.date()]).optional(),
-		tags: z.array(z.string()),
-	}),
-});
-
-export const collections = { posts, projects };
+export const collections = { posts };
