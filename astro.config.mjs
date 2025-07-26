7517104aa2ca23f56976acb835f110224ee8d21e
alejandro bernal <Richard7987@users.noreply.github.com>
2025-07-25 18:11:06 -0600

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-	site: "https://blahg.netlify.app/",
-	base: "/",
-	integrations: [sitemap(), react()],
-	markdown: {
-		shikiConfig: {
-			theme: "material-theme-darker",
-			langs: [],
-		},
-	},
+  site: "https://blahg.netlify.app/",
+  base: "/",
+  integrations: [sitemap(), react()],
+  markdown: {
+    shikiConfig: {
+      theme: "material-theme-darker",
+      langs: [],
+      wrap: true, // Mejora el renderizado de código
+    },
+    remarkPlugins: [], // Puedes añadir plugins de remark si los necesitas
+    rehypePlugins: [], // Puedes añadir plugins de rehype si los necesitas
+  },
 });
